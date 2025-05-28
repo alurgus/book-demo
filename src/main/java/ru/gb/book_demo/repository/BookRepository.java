17af2dc27292812e3e26e769cba43a2d5c13aae5
alurg <alurgus@mail.ru>
2025-05-28 17:56:36 +0300

    getBookById added

--- src/main/java/ru/gb/book_demo/repository/BookRepository.java
+++ src/main/java/ru/gb/book_demo/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package ru.gb.book_demo.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import ru.gb.book_demo.model.Book;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 
@@ -13,5 +15,20 @@ public class BookRepository {
 
     public BookRepository() {
         this.books = new ArrayList<>();
+    }
+
+    @PostConstruct
+    public void generatedData(){
+        books.addAll(List.of(
+                new Book("война и мир"),
+                new Book("нахаленок"),
+                new Book("му-му")
+        ));
+    }
+    public Book getBookById(long id){
+        return books.stream()
+                .filter(it -> Objects.equals(it.getId(),id))
+                .findFirst()
+                .orElse(null);
     }
 }
